0c91aea51cc65bd9b228f06d4517b300d79ae68e
Matt Stillwell <stillwell006@gmail.com>
2021-02-12 16:27:51 -0500

    Dark, pages

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from './Home';
+import { DetailsScreen } from './Details';
+
+const { Navigator, Screen } = createStackNavigator();
+
+const HomeNavigator = () => (
+  <Navigator headerMode="none">
+    <Screen name="Home" component={HomeScreen} />
+    <Screen name="Details" component={DetailsScreen} />
+  </Navigator>
+);
+
+export const AppNavigator = () => (
+  <NavigationContainer>
+    <HomeNavigator />
+  </NavigationContainer>
+);
